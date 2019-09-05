b5677093c5dd14e7bf95daa7b29a75cf35a11d77
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-09-04 22:44:10 -0400

    (WIP) - created ui and js file. Need to check DB entries

---
+++ logic.js
@@ -0,0 +1,74 @@
+var config = {
+    apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
+    authDomain: "time-sheet-55009.firebaseapp.com",
+    databaseURL: "https://time-sheet-55009.firebaseio.com",
+    storageBucket: "time-sheet-55009.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  $("#train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
+    var trainFrequency = $("#frequency-input").val().trim();
+  
+    // Creates local "temporary" object for holding train info
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      start: trainStart,
+      frequency: trainFrequency,
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+  });
+  
+  // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainStart = childSnapshot.val().start;
+    var trainFrequency = childSnapshot.val().frequency;
+  
+    // Prettify 
+    var trainStartPretty = moment.unix(trainStart).format("HH:mm");
+  
+    
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainStartPretty),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(),
+      $("<td>").text()
+    );
+  
+    // Append the new row to the table
+    $("#employee-table > tbody").append(newRow);
+  });
+  
